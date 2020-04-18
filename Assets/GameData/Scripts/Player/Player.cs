697ddbc1826daa69e075c6961c306eef39a442e9
Sascha Moos <sascha.moos@protonmail.com>
2020-04-18 15:09:20 +0200

    Added Configuration for Environmental Damage, Player now can Freeze and Radiate

--- Assets/GameData/Scripts/Player/Player.cs
+++ Assets/GameData/Scripts/Player/Player.cs
@@ -15,11 +15,36 @@ namespace KeepItAlive.Player
         public Sprite Sprite => _sprite;
 
         public float Health => _health;
-    
+
+        public EnvironmentalDamageConfiguration _environmentalDamageConfiguration;
+
+        public void Update()
+        {
+            if(DieCondition())
+            {
+                Die();
+            }
+        }
+        public void Freeze()
+        {
+            _health -= _environmentalDamageConfiguration.FreezeDamagePerTick;
+        }
+
+        public void Radiate()
+        {
+            _health -= _environmentalDamageConfiguration.RadiationDamagePerTick;
+        }
+
         public void Die()
         {
             DeathAnimation();
             Destroy(gameObject);
+        }
+
+        private bool DieCondition()
+        {
+            //TODO: Discuss and probably more to come
+            return _health >= 0.0f;
         }
 
         private void DeathAnimation()
---
+++ Assets/GameData/Scripts/Shared/EnvironmentalDamageConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeepItAlive.Shared
+{
+    public class EnvironmentalDamageConfiguration : MonoBehaviour
+    {
+        [SerializeField]
+        private float _freezeDamagePerTick;
+
+        [SerializeField]
+        private float _radiationDamagePerTick;
+
+        public float FreezeDamagePerTick;
+
+        public float RadiationDamagePerTick;
+
+        private EnvironmentalDamageConfiguration _instance;
+
+        private void Awake()
+        {
+            // if the singleton hasn't been initialized yet
+            if (_instance != null && _instance != this) 
+            {
+                Destroy(gameObject);
+            }
+    
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
