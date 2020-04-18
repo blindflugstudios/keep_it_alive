2845666014b8820a80537c34b72fcfce1c2f83fe
Sascha Moos <sascha.moos@protonmail.com>
2020-04-18 14:35:16 +0200

    Use the GameData Scripts Folder

---
+++ Assets/GameData/Scripts/Shared/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace KeepItAlive.Shared
+{
+    public interface IEntity
+    {
+        //Intent: Force implementing classes to have this fields
+        Sprite Sprite { get; }
+        float Health { get; }
+
+        void Die();
+    }
+}
+
