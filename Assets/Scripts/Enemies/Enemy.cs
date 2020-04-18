c4a404fd48b6067a0475d8d3006d690cd76963d2
Sascha Moos <sascha.moos@protonmail.com>
2020-04-18 13:31:40 +0200

    Added Sample Enemy Prefab with Enemy class

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace KeepItAlive.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite _sprite;
+
+        [SerializeField]
+        private float _health;
+
+        [SerializeField]
+        private string _name;
+
+        [SerializeField]
+        private GameObject _deathItemPrefab;
+        
+        public Enemy(float initialHealth, string name)
+        {
+            _health = initialHealth;
+        }
+
+        private void Die()
+        {
+            DeahAnimation();
+            Destroy(gameObject);
+
+            //Turns into other object
+            Instantiate(_deathItemPrefab);
+        }
+
+        private void DeahAnimation()
+        {
+            //TODO: IMPLEMENT
+            throw new NotImplementedException();
+        }
+    }
+}
