783b6c2bf266257f0d4c51bf36677e1c5cd819d2
Miens <w.miesikowski@gmail.com>
2020-04-19 15:44:22 +0200

    adjusted colliders

--- Assets/GameData/Scripts/UI/WorldSpaceUi.cs
+++ Assets/GameData/Scripts/UI/WorldSpaceUi.cs
@@ -1,8 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WorldSpaceUi : MonoBehaviour
 {
     [SerializeField] private WorldSpaceLabel _labelPrefab;
+
+    public static WorldSpaceUi Instance;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     public WorldSpaceLabel GetLabel()
     {
