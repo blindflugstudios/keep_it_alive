d6ea0860012c167975009ff0bba03225383438bc
Miens <w.miesikowski@gmail.com>
2020-04-19 11:32:07 +0200

    added enemy movement

--- Assets/GameData/Scripts/AI/Pathfinding.cs
+++ Assets/GameData/Scripts/AI/Pathfinding.cs
@@ -4,10 +4,12 @@ using UnityEngine.AI;
 
 public class Pathfinding : MonoBehaviour
 {
+    [SerializeField] private float _additionalRotationSpeed;
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private float _jumpDistance;
     [SerializeField] private Vector2 _jumpFrequency;
     [SerializeField] private float _jumpTime;
+    [SerializeField] private LayerMask _layerMask;
     [SerializeField] private Transform _target;
 
     public bool ShouldNavigate;
@@ -19,7 +21,18 @@ public class Pathfinding : MonoBehaviour
 
     private void Update()
     {
-        if (ShouldNavigate && _target != null) _agent.SetDestination(_target.position);
+        if (ShouldNavigate && _target != null)
+        {
+            _agent.SetDestination(_target.position);
+            RotateTowardsTarget();
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        var lookrotation = _agent.steeringTarget - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookrotation),
+            _additionalRotationSpeed * Time.deltaTime);
     }
 
     private IEnumerator DodgeRoutine()
@@ -29,9 +42,20 @@ public class Pathfinding : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(_jumpFrequency.x, _jumpFrequency.y));
             if (ShouldNavigate)
             {
-                ShouldNavigate = false;
-                yield return Jump();
-                ShouldNavigate = true;
+                var dist = Vector2.Distance(transform.position, _target.position);
+                var hit = Physics2D.Raycast(transform.position, (_target.position - transform.position).normalized,
+                    dist, _layerMask.value);
+
+                if (hit.collider == null)
+                {
+                    var dot = Vector3.Dot(transform.forward, (_target.position - transform.position).normalized);
+                    if (dot > 0.5f)
+                    {
+                        ShouldNavigate = false;
+                        yield return Jump();
+                        ShouldNavigate = true;
+                    }
+                }
             }
         }
     }
@@ -41,10 +65,18 @@ public class Pathfinding : MonoBehaviour
         var timeElapsed = 0f;
         var startPos = transform.position;
         var endPos = startPos + (Random.value > 0.5f ? -1f : 1f) * _jumpDistance * transform.right;
-        while (timeElapsed < _jumpTime)
+        endPos += 2f * _jumpDistance*(_target.position - transform.position).normalized;
+        var dir = (endPos - startPos).normalized;
+        var dist = Vector3.Distance(startPos, endPos);
+        var hit = Physics2D.Raycast(startPos, dir, dist, _layerMask.value);
+        if (hit.collider != null && hit.collider.isTrigger == false)
+            endPos = new Vector3(hit.point.x, hit.point.y, transform.position.z);
+        var jumpTime = _jumpTime * (Vector3.Distance(startPos, endPos) / _jumpDistance);
+
+        while (timeElapsed < jumpTime)
         {
             timeElapsed += Time.deltaTime;
-            var desiredPos = Vector3.Lerp(startPos, endPos, timeElapsed / _jumpTime);
+            var desiredPos = Vector3.Lerp(startPos, endPos, timeElapsed / jumpTime);
 
             _agent.Warp(desiredPos);
             yield return null;
