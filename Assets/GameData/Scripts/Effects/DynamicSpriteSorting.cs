2c6ffd111b59b9b39c0fc169459781ddf6ab6bb6
Ardaurum <paszkowski.radek@gmail.com>
2020-04-19 16:37:33 +0200

    Sprite Order

---
+++ Assets/GameData/Scripts/Effects/DynamicSpriteSorting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace KeepItAlive.Effects
+{
+    public sealed class DynamicSpriteSorting : MonoBehaviour
+    {
+        private SpriteRenderer[] _renderers;
+
+        private void Awake()
+        {
+            _renderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+        
+        private void Update()
+        {
+            var sortOrder = 32767 - Mathf.RoundToInt((transform.position.y + 100.0f) * 100.0f);
+            for (var i = 0; i < _renderers.Length; i++)
+            {
+                _renderers[i].sortingOrder = sortOrder;
+            }
+        }
+    }
+}
---
+++ Assets/GameData/Scripts/Effects/StaticSpriteSorting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace KeepItAlive.Effects
+{
+    public sealed class StaticSpriteSorting : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            yield return null;
+            yield return null;
+            var renderers = GetComponentsInChildren<SpriteRenderer>();
+            var sortOrder = 32767 - Mathf.RoundToInt((transform.position.y + 100.0f) * 100.0f);
+            for (var i = 0; i < renderers.Length; i++)
+            {
+                renderers[i].sortingOrder = sortOrder;
+            }
+        }
+    }
+}
